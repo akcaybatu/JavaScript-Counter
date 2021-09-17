ee8f413ca638aa8cc3dbcf7ceb6a5de9379c8484
Batuhan <akcay.batu76@gmail.com>
2021-09-17 14:16:32 +0300

    Project Finished

---
+++ app.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+const value = document.querySelector("#val");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(item){
+    item.addEventListener('click', function(e) {
+       const styles = e.currentTarget.classList;
+       if (styles.contains("decrease")) {
+           count--;
+       }
+       else if (styles.contains("increase")) {
+           count++;
+       } 
+       else {
+           count = 0;
+       }
+
+       if (count > 0) {
+        value.style.color = "green";
+       }
+       if (count < 0) {
+        value.style.color = "red";
+       }
+       if(count === 0){
+        value.style.color = "#222";
+       }
+       value.textContent = count;    
+    });    
+});
